e91c6f76693ba8f0864a0a5a109f1c4e8691149c
TomaszP17 <tplucinski15@gmail.com>
2024-03-08 22:45:51 +0100

    Zmiana na galezi main

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,13 +6,13 @@ Console.WriteLine(GetAvg(new []{1,2,3,4,5}));
 
 static double GetAvg(int[] arr)
 {
-    int sum = 0;
+    int sumx = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        sum += arr[i];
+        sumx += arr[i];
     }
 
-    return sum / arr.Length;
+    return sumx / arr.Length;
 }
 
 static int getMaxValue(int[] arr)
